0123d0a9bc3642a302a17548103b48342d25fee2
mauricio-arcanjo <140445426+mauricio-arcanjo@users.noreply.github.com>
2025-11-03 18:44:06 -0500

    first commit

---
+++ src/test/java/com/mauarcanjo/campeonato_brasileiro/CampeonatoBrasileiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauarcanjo.campeonato_brasileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoBrasileiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
